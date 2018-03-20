6ed8263c300ed0eb992cb86c38d8cf4155762736
YuLiming <yuliming0531@outlook.com>
2018-03-20 10:09:49 +0800

    增加log

--- src/main/java/com/ylm/service/DcSysLogService.java
+++ src/main/java/com/ylm/service/DcSysLogService.java
@@ -8,23 +8,59 @@ import java.util.List;
 public interface DcSysLogService {
 
     /**
-     *
+     * 获取数据数量
      * @param example
      * @return
      */
     int countByExample(DcSysOperateLogExample example);
 
+    /**
+     * 删除数据
+     * @param example
+     * @return
+     */
     int deleteByExample(DcSysOperateLogExample example);
 
+    /**
+     * 根据id删除数据
+     * @param olId
+     * @return
+     */
     int deleteByPrimaryKey(Integer olId);
 
+    /**
+     * 新增数据
+     * @param record
+     * @return
+     */
     int insert(DcSysOperateLog record);
 
+    /**
+     * 获取所有数据
+     * @param example
+     * @return
+     */
     List<DcSysOperateLog> selectByExample(DcSysOperateLogExample example);
 
+    /**
+     * 通过id筛选数据
+     * @param olId
+     * @return
+     */
     DcSysOperateLog selectByPrimaryKey(Integer olId);
 
+    /**
+     * 更新数据
+     * @param record
+     * @param example
+     * @return
+     */
     int updateByExample(DcSysOperateLog record, DcSysOperateLogExample example);
 
+    /**
+     * 更新数据
+     * @param record
+     * @return
+     */
     int updateByPrimaryKey(DcSysOperateLog record);
 }
--- src/main/java/com/ylm/service/DcUserService.java
+++ src/main/java/com/ylm/service/DcUserService.java
@@ -7,15 +7,45 @@ import java.util.List;
 
 public interface DcUserService {
 
+    /**
+     * 通过id筛选数据
+     * @param userId
+     * @return
+     */
     DcUser selectByPrimaryKey(String userId);
 
+    /**
+     * 获取所有数据
+     * @param example
+     * @return
+     */
     List<DcUser> selectByExample(DcUserExample example);
 
+    /**
+     * 更新数据
+     * @param record
+     * @return
+     */
     int updateByPrimaryKey(DcUser record);
 
+    /**
+     * 删除数据
+     * @param userId
+     * @return
+     */
     int deleteByPrimaryKey(String userId);
 
+    /**
+     * 新增数据
+     * @param record
+     * @return
+     */
     int insert(DcUser record);
 
+    /**
+     * 删除数据
+     * @param userId
+     * @return
+     */
     int deleteByPrimaryKeys(List<Integer> userId);
 }
